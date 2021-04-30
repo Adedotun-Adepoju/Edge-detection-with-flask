8862b7a47b58f414663325bb5e6400d119db8a59
Adepoju Adedotun <d.e.adepoju@gmail.com>
2021-04-30 14:52:39 +0100

    final

---
+++ module/__init__.py
@@ -0,0 +1,20 @@
+import os
+import cv2
+from flask import Flask
+
+app = Flask(__name__, static_folder=os.path.join('static/images'))
+UPLOAD_FOLDER = os.path.join('static','images')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY']='a58d2762c5d24aee56d3621b'
+
+print(UPLOAD_FOLDER)
+
+
+def det_edge(img):
+    img_array = cv2.imread(img)
+    gray_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+    img_edges = cv2.Canny(gray_img, threshold1=100, threshold2=200)
+
+    return img_edges
+
+from module import route
---
+++ module/route.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+from module import app, det_edge
+from flask import render_template, redirect, url_for, request, send_from_directory,flash
+from module.forms import ImageForm
+from werkzeug.utils import secure_filename
+
+
+
+@app.route("/", methods=["GET","POST"])
+def upload_image():
+    form=ImageForm()
+    if request.method == 'POST':
+        if form.validate_on_submit:
+            img = form.image.data
+
+            filename = secure_filename(img.filename)
+            img.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            img_edges = det_edge(full_filename)
+            cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'],filename), img_edges)
+            return render_template("detect.html", form=form, filename=filename)
+
+    return render_template("home.html",form=form)
+
+
